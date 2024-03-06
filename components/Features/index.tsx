f9a888b0bc963353a40b79195dd7e3db3e961f66
Rishabh <rishabh.kumar2022a@vitstudent.ac.in>
2024-03-07 04:11:03 +0530

    updated content of features

--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -11,11 +11,6 @@ export function Feature() {
 
 export const automationServices = [
   {
-    title: "24/7 Availability",
-    description:
-      "The AI chatbots are operational round the clock, handling your calls beyond office hours.",
-  },
-  {
     title: "Error-Free Data Handling",
     description:
       "Custom trained on your business data, the AI bots gather and store accurate user information.",
@@ -40,29 +35,5 @@ export const automationServices = [
     description:
       "As the bots efficiently handle calls, your staff can focus on other important tasks, resulting in increased productivity.",
   },
-  {
-    title: "Retention and Loyalty",
-    description:
-      "By addressing customer queries instantly and accurately, customer retention and loyalty are improved.",
-  },
-  {
-    title: "Prompt and Precise",
-    description:
-      "The sophisticated AI technology ensures a fast and precise response on every call.",
-  },
-  {
-    title: "Scalability",
-    description:
-      "As your business grows, the AI chatbots can be easily upgraded to handle increased call volumes.",
-  },
-  {
-    title: "Enhanced Information Security",
-    description:
-      "The AI bots adhere to strict security protocols to safeguard all customer information.",
-  },
-  {
-    title: "Analytics and Reports",
-    description:
-      "The AI chatbots provide valuable insights from call data analytics which can be used in strategical decision-making.",
-  },
+ 
 ];
