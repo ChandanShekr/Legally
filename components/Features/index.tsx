893670403b86ebb0bbd89341caa79eb91d62c0c7
Rishabh <rishabh.kumar2022a@vitstudent.ac.in>
2024-03-15 01:50:17 +0530

    added routes

--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -12,27 +12,9 @@ export const projects = [
   {
     title: "Dashboard",
     description:
-      "Custom trained on your business data, the AI bots gather and store accurate user information.",
-  },
-  {
-    title: "Tailored Solutions",
-    description:
-      "The bots are designed specifically according to your business needs, making them unique to your call handling requirements.",
-  },
-  {
-    title: "Superior Customer Service",
-    description:
-      "The AI bots answer calls promptly and book appointments seamlessly, increasing the overall customer satisfaction.",
-  },
-  {
-    title: "Cost Efficiency",
-    description:
-      "By handling the call management, the AI bots reduce the need for a larger in-house team and save costs.",
-  },
-  {
-    title: "Increased Productivity",
-    description:
-      "As the bots efficiently handle calls, your staff can focus on other important tasks, resulting in increased productivity.",
+      "DEEZ NUTS",
+    link: "/dashboard",
+     
   },
   
  
